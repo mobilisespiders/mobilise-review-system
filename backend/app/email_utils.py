4a78e053ae2c6a3a8714252df823ea6debb37af5
Saran Ironman <138017333+sarany-07@users.noreply.github.com>
2026-08-18 18:35:08 +0530

    Updated the dynamic dates in the email_utils.py

--- backend/app/email_utils.py
+++ backend/app/email_utils.py
@@ -112,7 +112,7 @@ def send_html_email(to_email, subject, recipient_name, assigned_users):
                                         </tr>
                                         <tr>
                                             <td style="color: #4a5568; font-size: 14px; line-height: 22px; padding: 0 0 24px 0;">
-                                                You are requested to provide your feedback for the following team members for <b>April, May and June</b> using these links:
+                                                You are requested to provide your feedback for the following team members for <b>{review_period}</b> using these links:
                                             </td>
                                         </tr>
 
@@ -147,7 +147,7 @@ def send_html_email(to_email, subject, recipient_name, assigned_users):
                                                         <td style="padding: 0 0 12px 0; color: #4a5568; font-size: 14px; line-height: 22px;">Lowlights are points where there is an opportunity for the person to improve. These don't necessarily mean negatives, rather, they are advice that could help make them better. Please don't leave it without pointers.</td>
                                                     </tr>
                                                     <tr>
-                                                        <td style="padding: 0 0 12px 0; color: #4a5568; font-size: 14px; line-height: 22px;">Your input is valuable and will remain confidential. Please ensure it is submitted by the end of the day on <strong>29 July 2026</strong>.</td>
+                                                        <td style="padding: 0 0 12px 0; color: #4a5568; font-size: 14px; line-height: 22px;">Your input is valuable and will remain confidential. Please ensure it is submitted by the end of the day on <strong>{due_date}</strong>.</td>
                                                     </tr>
                                                     <tr>
                                                         <td style="padding: 0 0 12px 0; color: #4a5568; font-size: 14px; line-height: 22px;">Kindly note that no reminders will be sent, and failure to complete the feedback may impact your evaluation score.</td>
